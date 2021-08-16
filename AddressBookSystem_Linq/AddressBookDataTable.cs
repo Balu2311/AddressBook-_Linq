dd716061909aac118c878295d94ac3392d39c4fc
Vallem Balu <vbnreddy5@gmail.com>
2021-08-16 19:16:50 +0530

    Add Cantacts in ADBook Table

--- AddressBookSystem_Linq/AddressBookDataTable.cs
+++ AddressBookSystem_Linq/AddressBookDataTable.cs
@@ -26,7 +26,28 @@ namespace AddressBookSystem_Linq
             dataTable.Columns.Add(taleColumn7);
             var taleColumn8 = new DataColumn("Email");
             dataTable.Columns.Add(taleColumn8);
+            
+            dataTable.Rows.Add("Balu", "Reddy", "Lkdkr", "Ongl", "AP", "523115", "9876543210", "vbn@gmail.com");
+            dataTable.Rows.Add("Siva", "Reddy", "N.Peta", "Gunturu", "AP", "526321", "6547890567", "siava@gmail.com");
+            dataTable.Rows.Add("Dipika", "Pilli", "Chennai", "mahabaliPuram", "TN", "654342", "9632154564", "dipika@gmail.com");
+            dataTable.Rows.Add("Mahesh", "Babu", "Banjarahiils", "Hyderabad", "TS", "500562", "8529631523", "mb@gmail.com");
+            dataTable.Rows.Add("Vishal", "Gunni", "Marthali", "Banglore", "Karanataka", "500056", "9632544570", "vishal@gmail.com");
+            dataTable.Rows.Add("Ganesh", "Reddy", "vesarpadi", "Chennai", "TamilNadu", "600341", "97456321011", "reddy@gmail.com");
             return dataTable;
+        }
+        public void Display()
+        {
+            foreach (var table in dataTable.AsEnumerable())
+            {
+                Console.WriteLine("\nFirstName: " + table.Field<string>("First_Name"));
+                Console.WriteLine("LastName: " + table.Field<string>("Last_Name"));
+                Console.WriteLine("Address: " + table.Field<string>("Address"));
+                Console.WriteLine("City: " + table.Field<string>("City"));
+                Console.WriteLine("State: " + table.Field<string>("State"));
+                Console.WriteLine("ZipCode: " + table.Field<string>("Zip"));
+                Console.WriteLine("PhoneNumber: " + table.Field<string>("Phone_Number"));
+                Console.WriteLine("E-mail: " + table.Field<string>("Email"));
+            }
         }
     }
 }
